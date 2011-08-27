5374c01d4bca880e77e0905514244e3b97e62d12
James Halliday <mail@substack.net>
2011-08-27 02:13:30 -0700

    now using a canvas to draw source

--- browser/run.js
+++ browser/run.js
@@ -6,19 +6,32 @@ module.exports = function (src) {
     var b = bunker(src);
     var lines = src.split('\n');
     
-    var canvas = $('<canvas>').attr({
-        width : 600,
-        height : lines.length * 16 + 20
-    }).appendTo($('#player'));
-    
-    var heat = heatmap(canvas.get(0));
-    var ctx = canvas.get(0).getContext('2d');
-    
-    var div = $('<div>')
-        .addClass('source')
-        .text(src)
+    var canvas = $('<canvas>')
+        .addClass('heat')
+        .attr('width', 600)
+        .attr('height', lines.length * 20)
         .appendTo($('#player'))
+        .get(0)
     ;
+    
+    var heat = heatmap(canvas);
+    var ctx = canvas.getContext('2d');
+    
+    var scanvas = $('<canvas>')
+        .attr('width', 600)
+        .attr('height', 141)
+        .addClass('source')
+        .appendTo($('#player'))
+        .get(0)
+    ;
+    
+    var sctx = scanvas.getContext('2d');
+    sctx.fillStyle = 'white';
+    sctx.font = '16px monospace';
+    src.split('\n').forEach(function (line, i) {
+        sctx.fillText(line, 0, 20 * (i + 1));
+    });
+    //sctx.globalCompositeOperation = 'source-atop';
     
     b.on('node', function (node) {
         var x = node.start.col * 10 + 8;
