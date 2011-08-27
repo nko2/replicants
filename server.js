7a57d266370f2135e413e7c444de63da03bb75b9
James Halliday <mail@substack.net>
2011-08-26 18:34:19 -0700

    permission denied errors but mostly the data stuff is in place

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var argv = require('optimist').argv;
 
+var mkdirp = require('mkdirp');
+mkdirp(__dirname + '/data');
+
 var app = express.createServer();
 app.use(express.static(__dirname + '/static'));
 
@@ -12,4 +15,14 @@ var bundle = browserify({
 });
 app.use(bundle);
 
-app.listen(argv.port || 80)
+var upload = require('./lib/upload.js');
+app.use(function (req, res, next) {
+    if (req.method === 'PUT') {
+        upload(req, res);
+    }
+    else next()
+});
+
+var port = argv.port || 80;
+app.listen(port)
+console.log('Listening on :' + port);
