1487cef748ea38bffb163d3413cc3fd47b679824
James Halliday <mail@substack.net>
2011-08-27 06:09:47 -0700

    interface to list files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var argv = require('optimist').argv;
+var fs = require('fs');
 
 var mkdirp = require('mkdirp');
 mkdirp(__dirname + '/data', 0700);
@@ -26,6 +27,20 @@ app.use(function (req, res, next) {
 
 app.get(new RegExp('^/id/.+'), require('./lib/player.js'));
 app.use('/file', express.static(__dirname + '/data'));
+app.get(new RegExp('/files/([0-9a-f]+)'), function (req, res) {
+    var id = req.params[0];
+    fs.readdir(__dirname + '/data/' + id, function (err, files) {
+        if (err) {
+            res.statusCode = 500;
+            res.setHeader('content-type', 'text/plain');
+            res.end(err.toString());
+        }
+        else {
+            res.setHeader('content-type', 'application/json');
+            res.end(JSON.stringify(files));
+        }
+    });
+});
 
 var port = argv.port || 80;
 app.listen(port)
