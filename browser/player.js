d1697790c8dd9f521261085691d0a1276694e6d9
James Halliday <mail@substack.net>
2011-08-27 06:14:18 -0700

    can now load multiple files from a project

--- browser/player.js
+++ browser/player.js
@@ -3,25 +3,39 @@ var http = require('http-browserify');
 var path = require('path');
 var run = require('./run');
 
-$(window).ready(function () {
-    var id = path.basename(window.location.pathname);
+function get (uri, cb) {
     var src = '';
-    var opts = { path : '/file/' + id + '/file.js' };
+    var opts = { path : uri };
+    
+    var src = '';
     http.get(opts, function (res) {
         res.on('data', function (buf) {
             src += buf.toString();
         });
         
         res.on('end', function () {
-            var b = run(src);
-            
-            b.run({
-                setTimeout : function (fn, t) {
-                    return setTimeout.apply(null, arguments);
-                },
-                setInterval : function (fn, t) {
-                    return setInterval.apply(null, arguments);
-                }
+            cb(src);
+        });
+    });
+}
+
+$(window).ready(function () {
+    var id = path.basename(window.location.pathname);
+    var src = '';
+    
+    get('/files/' + id, function (files) {
+        JSON.parse(files).forEach(function (file) {
+            get('/file/' + id + '/' + file, function (src) {
+                var b = run(src);
+                
+                b.run({
+                    setTimeout : function (fn, t) {
+                        return setTimeout.apply(null, arguments);
+                    },
+                    setInterval : function (fn, t) {
+                        return setInterval.apply(null, arguments);
+                    }
+                });
             });
         });
     });
