50b215b9274a93e8a0293d19899573aca6ee3646
david wee <rook2pawn@gmail.com>
2011-08-27 04:25:10 -0700

    Pretty commit

--- browser/run.js
+++ browser/run.js
@@ -1,8 +1,8 @@
 var $ = require('jquery-browserify');
 var bunker = require('bunker');
 var heatmap = require('heatmap');
-
 module.exports = function (src) {
+	src = src.replace(/\t/g, '    ');
     var b = bunker(src);
     var lines = src.split('\n');
     
@@ -12,6 +12,32 @@ module.exports = function (src) {
     }).appendTo($('#player'));
     
     var heat = heatmap(canvas.get(0));
+
+heat.addPointMultiple = function(x,y, num) {
+	for (var i = 0; i < num; i++) {
+		this.addPoint(x,y);
+	}
+};		
+heat.line = function (startx, starty, endx, endy, thickness) {
+	if (endx == startx) { return undefined;}
+	if (startx > endx) {
+		var tx = endx;
+		var ty = endy;
+		endx = startx;
+		endy = starty;
+		startx = tx;
+		starty = ty;
+	}
+	this.addPointMultiple(startx,starty, Math.floor(thickness + (thickness *0.25)));
+	this.addPointMultiple(endx,endy, Math.floor(thickness + (thickness *0.25)));
+	var slope = (endy - starty) / (endx - startx);
+	var b = starty - (slope * startx);
+	for (var x = startx; x <= endx; x+=1) {
+		var y = (slope * x) + b;
+		this.addPointMultiple(x,y,1);
+	}	
+	
+};
     var ctx = canvas.get(0).getContext('2d');
     
     var div = $('<div>')
@@ -19,12 +45,37 @@ module.exports = function (src) {
         .text(src)
         .appendTo($('#player'))
     ;
-    
     b.on('node', function (node) {
-        var x = node.start.col * 10 + 8;
-        var y = node.start.line * 20 + 10;
-        heat.addPoint(x, y);
-        heat.draw();
+		var nodesrc = src.slice(node.start.pos, node.end.pos+1);
+		console.log(node.start);
+		console.log(nodesrc);
+		console.log("*********\n");	
+		var startx = node.start.col * 10 + 8;
+        var starty = node.start.line * 20 + 10;
+		var lines = nodesrc.split('\n');
+		lines.forEach(function(line,linenum) {
+			var endx = line.length * 10 + 8;
+			if ((linenum === lines.length-1) && (linenum !== 0)) {
+				var match = line.match(/\S/);
+				if (match) {
+					startx = 8 + (line.match(/\S/).index * 10);
+				} else {
+					startx = 8;
+				}
+				heat.line(startx, starty + (linenum*20), node.end.col*10 + 8,  starty + (linenum * 20),1);
+			} else if (linenum >= 1) {
+				var match = line.match(/\S/);
+				if (match) {
+					startx = 8 + (line.match(/\S/).index * 10);
+				} else {
+					startx = 8;
+				}
+				heat.line(startx,starty + (linenum*20), endx, starty + (linenum * 20),1);
+			} else {
+				heat.line(startx,starty, endx, starty ,1);
+			}
+		});
+		heat.draw();
     });
     
     return b;
